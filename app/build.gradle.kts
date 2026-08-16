ff5c9f1484e3800a0108a71d152d74411289fa6a
tzero32 <241232307+tzero32@users.noreply.github.com>
2026-08-16 22:00:17 +0200

    Update versionCode to 2 and versionName to 1.1 in build.gradle.kts for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "tedwester.convo"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
