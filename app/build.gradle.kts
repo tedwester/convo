8992bebea47a9b452a4cd32c5f0f54939e3f0f02
tzero32 <241232307+tzero32@users.noreply.github.com>
2026-08-20 12:47:26 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "tedwester.convo"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
