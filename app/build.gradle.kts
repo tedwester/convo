b0e50b0b4116d8d6c240f6e2b4fc43a4fc0ec3c6
tzero32 <241232307+tzero32@users.noreply.github.com>
2026-08-21 12:22:13 +0200

    Update versionCode to 4 and versionName to 1.0.3 in build.gradle.kts for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "tedwester.convo"
         minSdk = 29
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
